c174ff694d9e6bf6a3d6bda0ea45027c213c9c86
sriram <sriram250901@gmail.com>
2022-08-09 09:49:13 +0530

    ricey data

---
+++ client/src/components/mainpages/utils/loading/Loading.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import './loading.css'
+
+function Loading() {
+    return (
+        <div>
+            <body>
+  <div id="loader">
+    <div class="ls-particles ls-part-1"></div>
+    <div class="ls-particles ls-part-2"></div>
+    <div class="ls-particles ls-part-3"></div>
+    <div class="ls-particles ls-part-4"></div>
+    <div class="ls-particles ls-part-5"></div>
+    <div class="lightsaber ls-left ls-green"></div>
+    <div class="lightsaber ls-right ls-red"></div>
+  </div>
+  
+</body>
+        </div>
+    )
+}
+
+export default Loading
